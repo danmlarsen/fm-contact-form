83aa5c6a06050b8ef0b9f34da7269fe5c860ade0
danmlarsen <larsendanmarius@gmail.com>
2024-08-15 15:41:32 +0200

    Validation implemented

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const formEl = document.querySelector('.contact-us__form');
 const successMessageEl = document.querySelector('.success-message');
 
 const DISPLAY_SUCCESS_TIMEOUT_SECONDS = 10;
+const FORM_FIELDS = ['firstname', 'lastname', 'email', 'querytype', 'message', 'callback'];
 
 const validations = {
     firstname: value => !!value.trim(),
@@ -15,6 +16,8 @@ const validations = {
 const renderSuccessMessage = function () {
     successMessageEl.classList.add('success-message--show');
 
+    formEl.reset();
+
     setTimeout(() => successMessageEl.classList.remove('success-message--show'), 1000 * DISPLAY_SUCCESS_TIMEOUT_SECONDS);
 };
 
@@ -24,16 +27,24 @@ const dataIsValid = function (key, value, validations) {
 
 const formIsValid = function (form, validations) {
     let isValid = true;
-    const keys = ['firstname', 'lastname', 'email', 'querytype', 'message', 'callback'];
     const data = Object.fromEntries(new FormData(form));
 
-    keys.forEach(key => {
+    FORM_FIELDS.forEach(key => {
+        const element = document.querySelector(`.contact-us__${key}`);
+
         if (!dataIsValid(key, data[key], validations)) {
             isValid = false;
+            let validationMessage = 'This field is required.';
 
-            const element = document.querySelector(`.contact-us__${key}`);
+            if (key === 'email') validationMessage = 'Please enter a valid email address';
+            if (key === 'querytype') validationMessage = 'Please select a query type';
+            if (key === 'callback') validationMessage = 'To submit this form, please consent to being contacted';
+
             element.classList.add('form-group--invalid');
-            element.querySelector('.contact-us__validation-error').textContent = 'This field is required.';
+            element.querySelector('.contact-us__validation-error').textContent = validationMessage;
+        } else {
+            element.classList.remove('form-group--invalid');
+            element.querySelector('.contact-us__validation-error').textContent = '';
         }
     });
 
